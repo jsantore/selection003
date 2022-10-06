005820aed0c0584b6fe6be1961bce72164178e7b
jsantore <jsantore@bridgew.edu>
2022-10-06 11:54:36 -0400

    second short circuit operator

---
+++ shortCircuitError.py
@@ -0,0 +1,3 @@
+answer = int(input( "How old are you:"))
+if answer > 21 or sdfgearvg/dfrgerag > VAD:
+    print("Legal in the US")
--- shortcircuitDemo.py
+++ shortcircuitDemo.py
@@ -3,5 +3,8 @@ import comp151Helpers
 graduation_list =  ["stu dent", "all done", "fin ished"]
 student_name = input("what student do you want to check:")
 lower_name = student_name.lower()
-if comp151Helpers.get_credits_from_database() >=120 or lower_name in graduation_list:
+if lower_name in graduation_list or comp151Helpers.get_credits_from_database() >=120:
     print("Congrats, you are almost done!")
+# the slow version
+#if comp151Helpers.get_credits_from_database() >=120 or lower_name in graduation_list:
+#    print("Congrats, you are almost done!")
